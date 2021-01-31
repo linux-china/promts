95c7451da76671a8b41cb9f6098b178cc4cb7a5d
linux_china <libing.chen@gmail.com>
2021-01-30 18:12:34 -0800

    bug fix

--- src/gauge_test.ts
+++ src/gauge_test.ts
@@ -9,37 +9,23 @@ import { Gauge } from "./gauge.ts";
 Deno.test("gauge increment", async (): Promise<void> => {
     const gauge: Gauge = new Gauge("gauge_procs");
 
-    gauge.inc();
-    gauge.inc(5);
-    assertEquals(gauge.getTotal(), 6);
+    gauge.setRatio(() => {
+      return 1;
+    })
+    assertEquals(gauge.getRatio(), 1);
 
 
 });
 
-Deno.test("gauge decrement", async (): Promise<void> => {
-
-    const gauge: Gauge = new Gauge("dec_counter");
-
-    gauge.inc();
-    gauge.inc(5);
-    gauge.dec(2);
-    assertEquals(gauge.getTotal(), 4);
-
-
-    gauge.inc(-1);
-    assertEquals(gauge.getTotal(), 3);
-
-
-
-});
 
 Deno.test("gauge toString()", async (): Promise<void> => {
     const gauge: Gauge = new Gauge("dec_counter", { region: "US" });
 
-    gauge.inc(500);
+    gauge.setRatio(() => {
+      return 500;
+    })
 
     assertEquals(gauge.toString(), `dec_counter{region="US"} 500\n`);
-    new Gauge("dec:counter", { region: "US" }).inc();
 });
 
 
